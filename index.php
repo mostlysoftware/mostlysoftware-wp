677ee47766294932d5d5695927e584f3e23d3c86
mostlysoftware <41282222+mostlysoftware@users.noreply.github.com>
2023-12-17 16:47:53 -0600

    0.00001

---
+++ index.php
@@ -0,0 +1,39 @@
+<!doctype html><html><head>
+
+    <title>Mostly Software</title>
+    <meta name="description" content="My name is Arcael. I make software, websites, and games.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta property="og:image" content="assets/thumbnail.png">
+    <link rel="icon" type="image/png" href="assets/logo.png">
+    <link href="style.css?ver=4" rel="stylesheet">
+
+</head><body>
+    
+    <header id="header">
+        
+        <h1 class="logo"><span class="part-1">Mostly</span><span class="part-2">.software</span></h1>
+        
+        
+
+    </header>
+
+    <main>
+
+        <div class="info"> 
+            <p>My name is <strong>Arcael</strong>. I make software, websites, and games.</p>
+            <p>
+                Youtube: <a href="https://www.youtube.com/@mostlysoftware" shadow="@MostlySoftware">@MostlySoftware</a></p>
+            <p>
+                Twitter: <a href="https://twitter.com/mostlysoftware" shadow="@MostlySoftware">@MostlySoftware</a> 
+                | <a href="https://twitter.com/messages/compose?recipient_id=117815273" class="twitter-dm-button" data-screen-name="@mostlysoftware" shadow="&#9993;">&#9993;</a></p>
+            <p>
+                Ko-fi: <a href="https://ko-fi.com/aracel" shadow="Arcael">Arcael</a></p>
+        </div>
+
+    </main>
+
+    
+<script src="script.js"></script>
+
+
+</body></html>
---
+++ script.js
@@ -0,0 +1,6 @@
+let channel_id = "UC-erCAm5wzcIke4WPUsqWh";
+let youtube_embed = '<iframe class="player" type="text/html" width="854" height="480" src="https://www.youtube.com/embed/live_stream?channel=' + channel_id + '&autoplay=0"></iframe>';
+
+
+
+document.getElementById("header").innerHTML += youtube_embed;
